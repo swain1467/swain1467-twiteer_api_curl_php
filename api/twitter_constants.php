31c0a9a6145a3397d46cb4f5138cc3e611f8a6b8
Rakesh <rakesh.das@mindfiresolutions.com>
2022-09-21 15:18:07 +0530

    Added constants

---
+++ api/twitter_constants.php
@@ -0,0 +1,5 @@
+<?php
+
+define('DEFAULT_HANDLE', 'imVkohli');
+define('BEARER_TOKEN', 'AAAAAAAAAAAAAAAAAAAAAGyRhAEAAAAAWJsG8OJonhgIrPNFaudb%2Fm7PqC0%3DWN77wad5uh5R81n9Cq8LGBpPwncD2haLKfiK4utt3nCSNCSH7P');
+define('API_ENDPOINT', 'https://api.twitter.com/2/');
